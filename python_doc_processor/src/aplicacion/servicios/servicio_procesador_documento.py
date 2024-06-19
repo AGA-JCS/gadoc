305bfc2bfce3ecb1e939891ff03fd7bf6ab3023d
Juan Carlos Martín Salinas Orellana <juan.salinas@vanda.cl>
2024-06-19 16:44:35 -0400

    REF Refactoriza el codigo del servicio de documentos de origen
    - Añade control de excepciones en los principales bloques
    - Incorpora Respuestas a los servicios expuestos
    - Incorpora el uso de DTOs en los parametros de entrada

--- python_doc_processor/src/aplicacion/servicios/servicio_procesador_documento.py
+++ python_doc_processor/src/aplicacion/servicios/servicio_procesador_documento.py
@@ -14,6 +14,15 @@ import vertexai
 import vertexai.preview.generative_models as generative_models
 from vertexai.generative_models import FinishReason, GenerativeModel, Part
 
+from src.aplicacion.dominio.servicios.respuestas import (
+    ResponseFailure,
+    ResponseSuccess,
+    ResponseTypes,
+)
+from src.aplicacion.dtos.documento import ParametrosExtraccionDTO
+from src.aplicacion.puertos.primarios.servicio_procesador_documento import (
+    IServicioProcesadorDocumentos,
+)
 from src.aplicacion.servicios.prompt import (
     promptBL,
     promptCarta,
@@ -33,9 +42,9 @@ location_ai = config["LOCATION"]
 llm = config["LLM"]
 
 
-class ServicioProcesadorDocumentos:
+class ServicioProcesadorDocumentos(IServicioProcesadorDocumentos):
 
-    def obtener_prompt(self, tipo_documento) -> str:
+    def _obtener_prompt(self, tipo_documento) -> str:
         prompt_map = {
             "BL": promptBL,
             "Carta": promptCarta,
@@ -46,48 +55,114 @@ class ServicioProcesadorDocumentos:
         # Obtenemos el prompt según el tipo de documento
         try:
             prompt = prompt_map.get(tipo_documento)
-        except:
+        except Exception as e:
             logger.exception("Prompt no encontrado para el tipo de documento", tipo_documento=tipo_documento)
+            raise e
         else:
             return prompt
 
-    def query_llm(self, full_path, tipo_documento):
-        prompt = self.obtener_prompt(tipo_documento)
-        document = Part.from_uri(mime_type="application/pdf", uri=full_path)
+    def obtener_parametros_procesamiento(self, tipo_documento: str, full_path_doc: str) -> dict:
+        try:
+            prompt = self._obtener_prompt(tipo_documento)
+        except Exception as e:
+            logger.exception(
+                "No se puede procesar el tipo de documento", tipo_documento=tipo_documento, full_path_doc=full_path_doc
+            )
+            raise e
+        else:
+            try:
+                documento = Part.from_uri(mime_type="application/pdf", uri=full_path_doc)
+            except Exception as e:
+                logger.exception(
+                    "No se puede procesar el documento", tipo_documento=tipo_documento, full_path_doc=full_path_doc
+                )
+                raise e
+            else:
+                return {
+                    "prompt": prompt,
+                    "document": documento,
+                    "generation_config": {
+                        "max_output_tokens": 8192,  # Limitar cuantos token el modelo puede dar como respuesta (la respuesta se corta en esa cantidad de tokens)
+                        "temperature": 1,  # Va de 0 a 2, siendo 0 respuestas deterministicas, y 2 de alta variabilidad.
+                        # "top_p": 0.95, # Similar a temperatura, permite indicar al modelo que solo elija
+                    },
+                    "safety_settings": {
+                        generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+                        generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+                        generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+                        generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+                    },
+                }
 
-        generation_config = {
-            "max_output_tokens": 8192,  # Limitar cuantos token el modelo puede dar como respuesta (la respuesta se corta en esa cantidad de tokens)
-            "temperature": 1,  # Va de 0 a 2, siendo 0 respuestas deterministicas, y 2 de alta variabilidad.
-            # "top_p": 0.95, # Similar a temperatura, permite indicar al modelo que solo elija
-        }
-        safety_settings = {
-            generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-            generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-            generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-            generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        }
+    def procesar_documento(self, parametros: dict) -> json:
+        prompt: str = parametros["prompt"]
+        document: Part = parametros["document"]
+        generation_config: dict = parametros["generation_config"]
+        safety_settings: dict = parametros["safety_settings"]
+        logger.debug("Iniciando VertexAI", project=project_id, location=location_ai)
+        try:
+            vertexai.init(project=project_id, location=location_ai)
+        except Exception as e:
+            logger.exception("No se puede iniciar VertexAI", project=project_id, location=location_ai)
+            raise e
+        else:
+            logger.debug("Iniciando modelo", llm=llm)
+            try:
+                model = GenerativeModel(llm)
+            except Exception as e:
+                logger.exception("No se puede iniciar el modelo", llm=llm)
+                raise e
+            else:
+                logger.debug(f"Extrayendo informacion", prompt=prompt, document=document)
+                try:
+                    responses = model.generate_content(
+                        [
+                            document,
+                            prompt,
+                        ],  # Se entrega los adjuntos (pdfs, imagenes, videos, etc), junto con la instrucción de texto en un listado
+                        generation_config=generation_config,  # Setting de parametros como top-p, top-k, temperatura, etc.
+                        safety_settings=safety_settings,  # Settings de seguridad
+                        stream=False,
+                    )
+                except Exception as e:
+                    logger.exception("No se puede procesar el documento", prompt=prompt, document=document)
+                    raise e
+                else:
+                    logger.debug("Documento procesado", respuesta=responses.text)
+                    return responses.text
 
-        return self.procesar_documento(document, prompt, generation_config, safety_settings)
+    ###########################################################################
+    # Métodos de la interfaz
+    ###########################################################################
+    def extraer_datos(self, parametros_extraccion: ParametrosExtraccionDTO) -> ResponseSuccess | ResponseFailure:
 
-    def procesar_documento(self, document, prompt, generation_config, safety_settings) -> json:
-        vertexai.init(project=project_id, location=location_ai)
-        print(f"Project : {project_id}")
-        print(f"Location : {location_ai}")
-        print(f"Llm : {llm}")
-        print(f"Promt: {prompt}")
-        print(f"Document: {document}")
-        model = GenerativeModel(llm)
-        responses = model.generate_content(
-            [
-                document,
-                prompt,
-            ],  # Se entrega los adjuntos (pdfs, imagenes, videos, etc), junto con la instrucción de texto en un listado
-            generation_config=generation_config,  # Setting de parametros como top-p, top-k, temperatura, etc.
-            safety_settings=safety_settings,  # Settings de seguridad
-            stream=False,
-        )
-        print(responses.text)
-        return responses.text
+        # 1. Obtener parametros de procesamiento
+        try:
+            parametros = self.obtener_parametros_procesamiento(
+                parametros_extraccion.tipo_documento, parametros_extraccion.full_path_doc
+            )
+        except Exception as e:
+            logger.exception(
+                "No se puede obtener los parametros de procesamiento",
+                tipo_documento=parametros_extraccion.tipo_documento,
+                full_path_doc=parametros_extraccion.full_path_doc,
+            )
+            return ResponseFailure(ResponseTypes.PARAMETERS_ERROR, e)
+        else:
+            # 2. Procesar documento
+            try:
+                respuesta = self.procesar_documento(parametros)
+            except Exception as e:
+                logger.exception(
+                    "No se puede procesar el documento",
+                    tipo_documento=parametros_extraccion.tipo_documento,
+                    full_path_doc=parametros_extraccion.full_path_doc,
+                )
+                return ResponseFailure(ResponseTypes.SYSTEM_ERROR, e)
+            else:
+                return ResponseSuccess(value=respuesta)
 
-    # def obtener_propiedades(self, id_despacho: int, full_path: str, tipo_documento: str):
-    #     prompt = self.obtener_prompt(tipo_documento = tipo_documento)
+    def cargar_documento(
+        self, numero_despacho: int, tipo_documento: str, full_path_doc: str
+    ) -> ResponseSuccess | ResponseFailure:
+        return super().cargar_documento(numero_despacho, tipo_documento, full_path_doc)
